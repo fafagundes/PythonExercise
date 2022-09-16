afb15dd20ee43577fe30ff8bb0b6527f2db25f28
FABRICIO <fabricio.mgit@gmail.com>
2022-09-15 23:27:08 -0300

    Update

--- BEECROWD - Exercises/BEGINNER/B-1079_Weighted_Averages.py
+++ BEECROWD - Exercises/BEGINNER/B-1079_Weighted_Averages.py
@@ -8,10 +8,21 @@ Output
 For each test case, print the weighted average according with below example.
 """
 
-N = int(input())
+def weighted_averages(N):
 
-for c in range(0, N):
-    W = list(map(float, input().split()))
-    AV = (((W[0]*2) + (W[1]*3) + (W[2]*5)) / 10)
-    print('{:.1f}'.format(AV))
+    for i in range(N):
+        v_test_1, v_test_2, v_test_3 = list(map(float, input().split()))
+        result = (((v_test_1 * 2) + (v_test_2 * 3) + (v_test_3 * 5)) / 10)
+        print(f"{result:.1f}")
+
+weighted_averages(int(input()))
+
+
+
+# N = int(input())
+#
+# for c in range(0, N):
+#     W = list(map(float, input().split()))
+#     AV = (((W[0]*2) + (W[1]*3) + (W[2]*5)) / 10)
+#     print('{:.1f}'.format(AV))
 
